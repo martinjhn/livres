b46060ec327296a6fe4e606056373e86078a8f19
ZHAN ZHUN <846794521@qq.com>
2021-05-18 15:37:14 +0200

    ajouter fixtures pour category

--- src/DataFixtures/LivreFixtures.php
+++ src/DataFixtures/LivreFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categorie;
+use App\Entity\Livre;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,7 +11,22 @@ class LivreFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = \Faker\Factory::create('fr_FR');
 
+        // Création Catégorie
+        // ******************
+        for ($c = 1; $c <= 10; $c++) {
+            $category = new Categorie();
+            $category->setNom($faker->word());
+            $manager->persist($category);
+
+            // Création Livres
+            // ***************
+            //     $book = new Livre();
+            //     $book->setTitre($faker->word())
+            //         ->setAuteur($faker->word())
+            //         ->setAnnee()
+        }
 
         $manager->flush();
     }
