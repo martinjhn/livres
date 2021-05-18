08f382f018f3a0fa1b74cdb1e66af3e163f72bdf
AlanTorres <alan.torres77@outlook.com>
2021-05-18 11:06:10 +0200

    Création Entity Emprunt et relations

---
+++ src/Entity/Emprunt.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmpruntRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmpruntRepository::class)
+ */
+class Emprunt
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Livre::class, inversedBy="emprunts")
+     */
+    private $livre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLivre(): ?Livre
+    {
+        return $this->livre;
+    }
+
+    public function setLivre(?Livre $livre): self
+    {
+        $this->livre = $livre;
+
+        return $this;
+    }
+}
