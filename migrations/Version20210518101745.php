1469f315e5fbe15e8413e679c10af459811c4a0c
AlanTorres <alan.torres77@outlook.com>
2021-05-18 13:51:15 +0200

    Relation d'emprunt(livre et utilisateur)+migration

---
+++ migrations/Version20210518101745.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210518101745 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE emprunt (id INT AUTO_INCREMENT NOT NULL, livre_id INT DEFAULT NULL, emprunteur_id INT DEFAULT NULL, INDEX IDX_364071D737D925CB (livre_id), INDEX IDX_364071D7F0840037 (emprunteur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE emprunt ADD CONSTRAINT FK_364071D737D925CB FOREIGN KEY (livre_id) REFERENCES livre (id)');
+        $this->addSql('ALTER TABLE emprunt ADD CONSTRAINT FK_364071D7F0840037 FOREIGN KEY (emprunteur_id) REFERENCES utilisateur (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE emprunt');
+    }
+}
--- src/Entity/Emprunt.php
+++ src/Entity/Emprunt.php
@@ -27,6 +27,16 @@ class Emprunt
      */
     private $emprunteur;
 
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_emprunt;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_retour;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +62,30 @@ class Emprunt
     public function setEmprunteur(?Utilisateur $emprunteur): self
     {
         $this->emprunteur = $emprunteur;
+
+        return $this;
+    }
+
+    public function getDateEmprunt(): ?\DateTimeInterface
+    {
+        return $this->date_emprunt;
+    }
+
+    public function setDateEmprunt(\DateTimeInterface $date_emprunt): self
+    {
+        $this->date_emprunt = $date_emprunt;
+
+        return $this;
+    }
+
+    public function getDateRetour(): ?\DateTimeInterface
+    {
+        return $this->date_retour;
+    }
+
+    public function setDateRetour(\DateTimeInterface $date_retour): self
+    {
+        $this->date_retour = $date_retour;
 
         return $this;
     }
