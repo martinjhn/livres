065fea0f9c5afd0916ae8a906daaf26b10cf7cbd
MartinJ <yaronan@yahoo.com>
2021-05-18 17:30:51 +0200

    Ajout des autorisations selon le role

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,4 +18,21 @@ class UtilisateurController extends AbstractController
             'controller_name' => 'UtilisateurController',
         ]);
     }
+
+    /**
+     * @IsGranted("ROLE_ADMIN", StatusCode=401, message="Accès refusé à cette page")
+    */
+    public function editForm() {
+        try {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        }
+        catch(AccessDeniedException $ex) {
+            $this->addFlash('error', "Vous n'avez pas accès à cette ressource");
+        }
+        finally 
+        {
+            return $this->redirectToRoute("#") ;
+        }
+    }
+
 }
