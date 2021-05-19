38bc110c22306fe753b9e5beed7443c1856e0d57
ZHAN ZHUN <846794521@qq.com>
2021-05-19 09:39:53 +0200

    creation fixtures pour utilisateur

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,5 +10,22 @@ class UtilisateurFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        // Création Utilisateur
+        // ********************
+        for ($u = 1; $u <= 20; $u++) {
+            $utilisateur = new Utilisateur();
+            $utilisateur->setNom($faker->lastName())
+                ->setPrenom($faker->firstName())
+                ->setAdresse($faker->address())
+                ->setTel($faker->e164PhoneNumber())
+                ->setCourriel($faker->email())
+                ->setMotDePasse($faker->password())
+                ->setConfirm($faker->password());
+            $manager->persist($utilisateur);
+        }
+
+        $manager->flush();
     }
 }
