032abe54174eb1d858a16712fbe62c67fb2e01eb
ZHAN ZHUN <846794521@qq.com>
2021-05-20 13:53:11 +0200

    corriger [oublier supprimer route situ au dessus de class UtilisateurController]

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -11,9 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-/**
- * @Route("/utilisateur")
- */
+
 class UtilisateurController extends AbstractController
 {
 	/**
