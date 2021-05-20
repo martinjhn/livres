1113cee485a6fed38d5dc94a0cb2bccff6c15179
Nathanael Lafeuille <akai.chesha@gmail.com>
2021-05-20 13:07:25 +0200

    Ajoute l'auth & le CRUD pour l'Utilisateur

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,61 +11,61 @@ use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        $faker = \Faker\Factory::create('fr_FR');
+	public function load(ObjectManager $manager)
+	{
+		$faker = \Faker\Factory::create('fr_FR');
 
-        $listeUtilisateur = [];
-        // Création Utilisateur
-        // ********************
-        for ($u = 1; $u <= 5; $u++) {
-            $mdp = $faker->password();
-            $utilisateur = new Utilisateur();
-            $utilisateur->setNom($faker->lastName())
-                ->setPrenom($faker->firstName())
-                ->setAdresse($faker->address())
-                ->setTel($faker->e164PhoneNumber())
-                ->setCourriel($faker->email())
-                ->setMotDePasse($mdp)
-                ->setConfirm($mdp);
-            $listeUtilisateur[] = $utilisateur;
-            $manager->persist($utilisateur);
-        }
+		$listeUtilisateur = [];
+		// Création Utilisateur
+		// ********************
+		for ($u = 1; $u <= 5; $u++) {
+			$mdp = $faker->password();
+			$utilisateur = new Utilisateur();
+			$utilisateur->setNom($faker->lastName())
+				->setPrenom($faker->firstName())
+				->setAdresse($faker->address())
+				->setTel($faker->e164PhoneNumber())
+				->setEmail($faker->email())
+				->setPassword($mdp)
+				->setConfirm($mdp);
+			$listeUtilisateur[] = $utilisateur;
+			$manager->persist($utilisateur);
+		}
 
-        // Création Catégorie
-        // ******************
-        for ($c = 1; $c <= 5; $c++) {
-            $category = new Categorie();
-            $category->setNom($faker->word());
-            $manager->persist($category);
+		// Création Catégorie
+		// ******************
+		for ($c = 1; $c <= 5; $c++) {
+			$category = new Categorie();
+			$category->setNom($faker->word());
+			$manager->persist($category);
 
-            // Création Livres
-            // ***************
-            for ($l = 1; $l <= 5; $l++) {
-                $livre = new Livre();
-                $livre->setTitre($faker->word())
-                    ->setAuteur($faker->word())
-                    ->setAnnee($faker->year($max = 'now'))
-                    ->setCategorie($category);
-                $manager->persist($livre);
+			// Création Livres
+			// ***************
+			for ($l = 1; $l <= 5; $l++) {
+				$livre = new Livre();
+				$livre->setTitre($faker->word())
+					->setAuteur($faker->word())
+					->setAnnee($faker->year($max = 'now'))
+					->setCategorie($category);
+				$manager->persist($livre);
 
-                // Création Emprunt
-                // ****************
-                for ($e = 1; $e <= 5; $e++) {
-                    $currUser = $listeUtilisateur[array_rand($listeUtilisateur, 1)];
-                    $emprunt = new Emprunt();
+				// Création Emprunt
+				// ****************
+				for ($e = 1; $e <= 5; $e++) {
+					$currUser = $listeUtilisateur[array_rand($listeUtilisateur, 1)];
+					$emprunt = new Emprunt();
 
-                    $emprunt->setLivre($livre)
-                        ->setEmprunteur($currUser)
-                        ->setDateEmprunt($faker->dateTimeBetween("-2 months"));
-                    $nbJours = (new \DateTime())->diff($emprunt->getDateEmprunt())->days;
-                    $emprunt
-                        ->setDateRetour($faker->dateTimeBetween("-" . $nbJours . "days", "+" . $nbJours . "days"));
-                    $manager->persist($emprunt);
-                }
-            }
+					$emprunt->setLivre($livre)
+						->setEmprunteur($currUser)
+						->setDateEmprunt($faker->dateTimeBetween("-2 months"));
+					$nbJours = (new \DateTime())->diff($emprunt->getDateEmprunt())->days;
+					$emprunt
+						->setDateRetour($faker->dateTimeBetween("-" . $nbJours . "days", "+" . $nbJours . "days"));
+					$manager->persist($emprunt);
+				}
+			}
 
-            $manager->flush();
-        }
-    }
+			$manager->flush();
+		}
+	}
 }
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -3,183 +3,237 @@
 namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class Utilisateur
+class Utilisateur implements UserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $nom;
+	/**
+	 * @ORM\Column(type="string", length=180, unique=true)
+	 */
+	private $email;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $prenom;
+	/**
+	 * @ORM\Column(type="json")
+	 */
+	private $roles = [];
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $adresse;
+	/**
+	 * @var string The hashed password
+	 * @ORM\Column(type="string")
+	 */
+	private $password;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $nom;
 
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $tel;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $prenom;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $courriel;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $adresse;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $motDePasse;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $tel;
+	private $confirm;
+	/**
+	 * @ORM\OneToMany(targetEntity=Emprunt::class, mappedBy="emprunteur")
+	 */
+	private $emprunts;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $confirm;
+	public function __construct()
+      	{
+      		$this->emprunts = new ArrayCollection();
+      	}
 
-    /**
-     * @ORM\OneToMany(targetEntity=Emprunt::class, mappedBy="emprunteur")
-     */
-    private $emprunts;
+	public function getId(): ?int
+      	{
+      		return $this->id;
+      	}
 
-    public function __construct()
-    {
-        $this->emprunts = new ArrayCollection();
-    }
+	public function getEmail(): ?string
+      	{
+      		return $this->email;
+      	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function setEmail(string $email): self
+      	{
+      		$this->email = $email;
+      
+      		return $this;
+      	}
 
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
+	/**
+	 * A visual identifier that represents this user.
+	 *
+	 * @see UserInterface
+	 */
+	public function getUsername(): string
+      	{
+      		return (string) $this->email;
+      	}
 
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
+	/**
+	 * @see UserInterface
+	 */
+	public function getRoles(): array
+      	{
+      		$roles = $this->roles;
+      		// guarantee every user at least has ROLE_USER
+      		$roles[] = 'ROLE_USER';
+      
+      		return array_unique($roles);
+      	}
 
-        return $this;
-    }
+	public function setRoles(array $roles): self
+      	{
+      		$this->roles = $roles;
+      
+      		return $this;
+      	}
 
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function getPassword(): string
+      	{
+      		return $this->password;
+      	}
 
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
+	public function setPassword(string $password): self
+      	{
+      		$this->password = $password;
+      
+      		return $this;
+      	}
 
-        return $this;
-    }
+	/**
+	 * Returning a salt is only needed, if you are not using a modern
+	 * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+	 *
+	 * @see UserInterface
+	 */
+	public function getSalt(): ?string
+      	{
+      		return null;
+      	}
 
-    public function getAdresse(): ?string
-    {
-        return $this->adresse;
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function eraseCredentials()
+      	{
+      		// If you store any temporary, sensitive data on the user, clear it here
+      		// $this->plainPassword = null;
+      	}
+	public function getConfirm(): ?string
+      	{
+      		return $this->confirm;
+      	}
 
-    public function setAdresse(string $adresse): self
-    {
-        $this->adresse = $adresse;
+	public function setConfirm(string $confirm): self
+      	{
+      		$this->confirm = $confirm;
+      
+      		return $this;
+      	}
 
-        return $this;
-    }
+	/**
+	 * @return Collection|Emprunt[]
+	 */
+	public function getEmprunts(): Collection
+      	{
+      		return $this->emprunts;
+      	}
 
-    public function getTel(): ?int
-    {
-        return $this->tel;
-    }
+	public function addEmprunt(Emprunt $emprunt): self
+      	{
+      		if (!$this->emprunts->contains($emprunt)) {
+      			$this->emprunts[] = $emprunt;
+      			$emprunt->setEmprunteur($this);
+      		}
+      
+      		return $this;
+      	}
 
-    public function setTel(?int $tel): self
-    {
-        $this->tel = $tel;
+	public function removeEmprunt(Emprunt $emprunt): self
+      	{
+      		if ($this->emprunts->removeElement($emprunt)) {
+      			// set the owning side to null (unless already changed)
+      			if ($emprunt->getEmprunteur() === $this) {
+      				$emprunt->setEmprunteur(null);
+      			}
+      		}
+      
+      		return $this;
+      	}
 
-        return $this;
-    }
+	public function getNom(): ?string
+      	{
+      		return $this->nom;
+      	}
 
-    public function getCourriel(): ?string
-    {
-        return $this->courriel;
-    }
+	public function setNom(string $nom): self
+      	{
+      		$this->nom = $nom;
+      
+      		return $this;
+      	}
 
-    public function setCourriel(string $courriel): self
-    {
-        $this->courriel = $courriel;
+	public function getPrenom(): ?string
+      	{
+      		return $this->prenom;
+      	}
 
-        return $this;
-    }
+	public function setPrenom(string $prenom): self
+      	{
+      		$this->prenom = $prenom;
+      
+      		return $this;
+      	}
 
-    public function getMotDePasse(): ?string
-    {
-        return $this->motDePasse;
-    }
+	public function getAdresse(): ?string
+      	{
+      		return $this->adresse;
+      	}
 
-    public function setMotDePasse(string $motDePasse): self
-    {
-        $this->motDePasse = $motDePasse;
+	public function setAdresse(string $adresse): self
+      	{
+      		$this->adresse = $adresse;
+      
+      		return $this;
+      	}
 
-        return $this;
-    }
+	public function getTel(): ?string
+      	{
+      		return $this->tel;
+      	}
 
-    public function getConfirm(): ?string
-    {
-        return $this->confirm;
-    }
-
-    public function setConfirm(string $confirm): self
-    {
-        $this->confirm = $confirm;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Emprunt[]
-     */
-    public function getEmprunts(): Collection
-    {
-        return $this->emprunts;
-    }
-
-    public function addEmprunt(Emprunt $emprunt): self
-    {
-        if (!$this->emprunts->contains($emprunt)) {
-            $this->emprunts[] = $emprunt;
-            $emprunt->setEmprunteur($this);
-        }
-
-        return $this;
-    }
-
-    public function removeEmprunt(Emprunt $emprunt): self
-    {
-        if ($this->emprunts->removeElement($emprunt)) {
-            // set the owning side to null (unless already changed)
-            if ($emprunt->getEmprunteur() === $this) {
-                $emprunt->setEmprunteur(null);
-            }
-        }
-
-        return $this;
-    }
+	public function setTel(string $tel): self
+      	{
+      		$this->tel = $tel;
+      
+      		return $this;
+      	}
 }
