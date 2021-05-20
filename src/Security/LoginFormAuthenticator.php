3d2ad48ebebef5db5e4d47534512acf6bdfe327c
ZHAN ZHUN <846794521@qq.com>
2021-05-20 13:43:29 +0200

    Modifie la route de redirection après connexion vers la liste des catégories

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -95,7 +95,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 			return new RedirectResponse($targetPath);
 		}
 
-		return new RedirectResponse($this->urlGenerator->generate('utilisateur_index'));
+		return new RedirectResponse($this->urlGenerator->generate('categorie_index'));
 		// throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
 	}
 
